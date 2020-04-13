369e1bc76da90aa25b16f73502ab60b4e4ead3be
Mevericks <reloved@gmail.com>
2020-04-13 19:44:55 +0300

    добавил трейт для User и middleware от spatie

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,8 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
