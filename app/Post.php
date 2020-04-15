930d7364c872e798a296d7190abf68fcbc855da5
Mevericks <reloved@gmail.com>
2020-04-15 18:52:50 +0300

    сохранение коммента через $this->comments->create(compact('body'))

--- app/Post.php
+++ app/Post.php
@@ -12,9 +12,6 @@ class Post extends Model
     }
     public function addComment($body)
     {
-		Comment::create([
-            'body' => $body,
-            'post_id' => $this->id
-        ]);
+    	$this->comments()->create(compact('body'));
     }
 }
