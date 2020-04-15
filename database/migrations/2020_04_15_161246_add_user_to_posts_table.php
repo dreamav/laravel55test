3c96dd9f3e626f53a059492674aa26a34319701b
Mevericks <reloved@gmail.com>
2020-04-15 19:18:12 +0300

    миграция добавил user_id

---
+++ database/migrations/2020_04_15_161246_add_user_to_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('user_id')->after('body');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('user_id')->after('post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('user_id')->after('body');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('user_id')->after('post_id');
+        });
+    }
+}
