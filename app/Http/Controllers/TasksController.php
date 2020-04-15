dd07761fd1dbc5431a32ee9b5d4bb52296d9f505
Mevericks <reloved@gmail.com>
2020-04-15 16:07:49 +0300

    add layout.blade.php

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -12,9 +12,9 @@ class TasksController extends Controller
 		$tasks = Task::all();
 	    return view('tasks.index', compact('tasks'));
     }
-    public function show($id)
+    public function show(Task $task) // Task::find(wildcard) 'tasks/{wildcard}'
     {
-		$task = Task::find($id);
+		// $task = Task::find($id);
 	    return view('tasks.show', compact('task'));
     }
 }
